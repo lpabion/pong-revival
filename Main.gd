dd375417daf2bc1c42385cd96aab0ad152a0f2b7
lpabion <lpabion@gmail.com>
2020-09-13 20:15:13 +0200

    rackets as scenes

--- Main.gd
+++ Main.gd
@@ -1,10 +1,34 @@
 extends Node2D
 
 var score = [0, 0]
+var racket = preload("res://racket/racket.tscn")
 
 func _init():
     randomize()
 
-func _ready():
-    pass
 
+func _ready():
+    spawn_rackets()
+    spawn_walls()
+
+func spawn_walls():
+    var top_wall = StaticBody2D.new()
+    var top_wall_poly = CollisionPolygon2D.new()
+    top_wall_poly.polygon = PoolVector2Array([Vector2(0, 0), Vector2(0, -16), Vector2(1024, -16), Vector2(1024, 0)])
+    top_wall.add_child(top_wall_poly)
+    self.add_child(top_wall)
+
+    var bottom_wall = StaticBody2D.new()
+    var bottom_wall_poly = CollisionPolygon2D.new()
+    bottom_wall_poly.polygon = PoolVector2Array([Vector2(0, 768), Vector2(1024, 768), Vector2(1024, 768), Vector2(0, 768)])
+    bottom_wall.add_child(bottom_wall_poly)
+    self.add_child(bottom_wall)
+
+func spawn_rackets():
+    var player_racket = racket.instance()
+    player_racket.initialize(Vector2(32, 384), Color(1, 0, 0))
+    self.add_child(player_racket)
+
+    var ai_racket = racket.instance()
+    ai_racket.initialize(Vector2(992, 384), Color(0, 1, 0))
+    self.add_child(ai_racket)
--- player/player.gd
+++ player/player.gd
@@ -6,6 +6,10 @@ var velocity = Vector2(0, 0)
 func _ready():
     pass # Replace with function body.
 
+func initialize(pos, color_mod):
+    self.position = pos
+    $racket_spr.modulate = Color(color_mod)
+
 func _process(delta):
     velocity = Vector2(0, 0)
     if Input.is_action_pressed("ui_up"):
